240662f10800a00bd75904c7abcba04cab6f504b
heartacker <heartacker@outlook.com>
2023-10-23 18:57:31 +0800

    font?

--- demo/Ursa.Demo.Desktop/Program.cs
+++ demo/Ursa.Demo.Desktop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Dialogs;
 using Avalonia.Media;
@@ -7,6 +8,18 @@ namespace Ursa.Demo.Desktop;
 
 class Program
 {
+
+    static string MacOS_Default_Font = "STHeiti";
+    static string Windows_Default_Font = "Microsoft YaHei";
+    static string Linux_Default_Font = "Noto Sans CJK SC";
+
+    static string DefaultFontName()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return Linux_Default_Font;
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) return MacOS_Default_Font;
+        return Windows_Default_Font;
+    }
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
@@ -18,7 +31,7 @@ class Program
             {
                 new FontFallback
                 {
-                    FontFamily = new FontFamily("Microsoft YaHei")
+                    FontFamily = new FontFamily(DefaultFontName())
                 }
             }
         })
