e7ec8d6387e2fa08ae7234d4314e7e6b354dbc58
heartacker <heartacker@outlook.com>
2024-04-07 15:26:20 +0800

    add reason

--- src/VariableBox/Controls/VariableBox/ValueChangedEventArgs.cs
+++ src/VariableBox/Controls/VariableBox/ValueChangedEventArgs.cs
@@ -1,15 +1,28 @@
 using Avalonia.Interactivity;
 
 namespace VariableBox.Controls;
-
-public class ValueChangedEventArgs<T>: RoutedEventArgs where T: struct, IComparable<T>
+public enum ValueChangedReason
 {
-    public ValueChangedEventArgs(RoutedEvent routedEvent, T? oldValue, T? newValue): base(routedEvent)
+    None = 0,
+    UserInput = 1,
+    OnSpinner = 2,
+    OnRead = 4,
+    OnWrite = 8,
+}
+
+public class ValueChangedEventArgs<T> : RoutedEventArgs where T : struct, IComparable<T>
+{
+    public ValueChangedEventArgs(RoutedEvent routedEvent, T? oldValue, T? newValue,
+        ValueChangedReason? reason = ValueChangedReason.None) : base(routedEvent)
     {
         OldValue = oldValue;
         NewValue = newValue;
+        Reason = reason;
     }
+
     public T? OldValue { get; }
+
     public T? NewValue { get; }
 
+    public ValueChangedReason? Reason { get; }
 }
